4eb041389eb1c0767f1dc720af95719fc3f47c46
DerekWW <derek.wierson@gmail.com>
2018-01-10 23:39:39 -0800

    remove passport

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,27 +1,7 @@
 const express = require('express')
 const knex = require('../../knex')
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth').OAuthStrategy
 const router = express.Router()
 
-passport.use(new GoogleStrategy({
-  consumerKey: process.env.GOOGLE_CONSUMER_KEY,
-  consumerSecret: process.env.GOOGLE_CONSUMER_SECRET,
-  callbackURL: '/api/home'
-}, (token, tokenSecret, profile, done) => {
-  //knex query here for user
-}))
 
-
-router.get('/google',
-  passport.authenticate('google', { scope: 'https://www.google.com/m8/feeds' })
- )
-
-router.get('/google/callback',
-passport.authenticate('google', { failureRedirect: '/asdf' }),
-  function(req, res) {
-    res.redirect('/');
-  }
-);
 
 module.exports = router
